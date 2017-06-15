9c4fc1dbde93f9e7a9e5f364aaa25b9f4092f2df
xyxiao <463618622@qq.com>
2017-06-15 18:02:38 +0800

    add some

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,8 +3,60 @@
 var app = getApp()
 Page({
   data: {
+    login: false,
+    userInfo: {},
+    shareTicket: null,
+    group: null
+  },
+  //分享
+  onShareAppMessage: function () {
+    return {
+      title: '快来投票哟',
+      path: '/pages/index',
+      success: function (res) {
+        // 转发成功
+        console.log(res.shareTickets)
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
   },
   onLoad: function () {
+    wx.showToast({
+      icon: 'loading',
+      title: '登录中..',
+    })
+    // 显示分享群
+    wx.showShareMenu({
+      withShareTicket: true
+    })
     console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo,
+        login: app.globalData.login
+      })
+      // wx.hideShowToast()
+      wx.showToast({
+        icon: 'success',
+        title: '登录成功..'
+      })
+
+      // 调用获取信息
+      if (app.globalData.shareTicket) {
+        that.setData({
+          shareTicket: app.globalData.shareTicket
+        })
+        app.getId(function (id) {
+          that.setData({
+            group: id
+          })
+        })
+      }
+    })
   }
 })
